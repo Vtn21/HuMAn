3d94aea8217a1d3cfc9c21cd5082f430d85b004c
Vtn21 <victortn21@hotmail.com>
2021-02-04 17:48:15 -0300

    Added TF Example converter

--- amass_tools/load_amass.py
+++ amass_tools/load_amass.py
@@ -10,11 +10,86 @@ Also, TFRecords are preferred over the tf.data.Dataset, as AMASS is large
 Author: Victor T. N.
 """
 
+
 import glob
 import numpy as np
 import os
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "1"  # Hide unnecessary TF messages
 import tensorflow as tf
+
+
+"""
+TFRecord conversion functions
+
+The following three functions convert standard TensorFlow types into TFRecord
+types: BytesList, FloatList and Int64List.
+
+https://www.tensorflow.org/tutorials/load_data/tfrecord
+"""
+
+
+def _bytes_feature(value):
+    # Returns a bytes_list from a string / byte.
+    if isinstance(value, type(tf.constant(0))):
+        # BytesList won't unpack a string from an EagerTensor.
+        value = value.numpy()
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+
+def _float_feature(value):
+    # Returns a float_list from a float / double.
+    return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
+
+
+def _int64_feature(value):
+    # Returns an int64_list from a bool / enum / int / uint.
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+
+"""
+Encoding functions
+
+Transform input data from AMASS recordings (numpy) into TF Examples, that are
+able to be packed inside a TFRecord.
+"""
+
+
+def amass_example(bdata, framerate_drop=1, max_betas=10):
+    """Create a TF Example from an AMASS recording.
+    There is the option to drop down the framerate (augmentation).
+
+    Args:
+        bdata (numpy dict): uncompressed numpy data from the recording.
+        framerate_drop (int, optional): Used to drop the original framerate
+                                        (data augmentation). Defaults to 1.
+        max_betas (int, optional): Limits the maximum number of shape
+                                   components. Defaults to 10.
+
+    Returns:
+        [tf.train.Example]: relevant data fields wrapped into TF Example.
+    """
+    # Turning string gender into integer
+    if str(bdata["gender"]) == "male":
+        gender_int = -1
+    elif str(bdata["gender"]) == "female":
+        gender_int = 1
+    else:
+        gender_int = 0
+    # Ensure that the number of betas is not greater than what is available
+    num_betas = min(max_betas, bdata["betas"].shape[0])
+    # Build feature dict
+    feature = {
+        # Keep only joint poses, discarding body orientation
+        # framerate_drop acts here, picking just part of the array
+        "poses": _float_feature(bdata["poses"][0::framerate_drop, 3:72]),
+        # Time interval between recordings, considering framerate drop
+        "dt": _float_feature(framerate_drop/bdata["mocap_framerate"]),
+        # Shape components (betas), up to the maximum defined amount
+        "betas": _float_feature(bdata["betas"][:num_betas]),
+        # Gender encoded into integer
+        "gender": _int64_feature(gender_int)
+    }
+    return tf.train.Example(features=tf.train.Features(feature=feature))
 
 
 if __name__ == "__main__":
@@ -42,6 +117,6 @@ if __name__ == "__main__":
                 if "poses" not in list(bdata.keys()):
                     continue
                 else:
-                    print("Recording: %s" % recording)
+                    print("Recording: %s" % npz_file)
                     print('Data keys available:%s' % list(bdata.keys()))
                 break
