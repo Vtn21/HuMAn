aa807878081b46b0d804179f82ce7072451c7262
Vtn21 <victortn21@hotmail.com>
2021-02-12 10:01:26 -0300

    Subnets successfully built for lower body

--- model/human.py
+++ model/human.py
@@ -9,16 +9,17 @@ Author: Victor T. N.
 
 import os
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "1"  # Hide unnecessary TF messages
-import tensorflow as tf  # noqa: E402
 from tensorflow import keras  # noqa: E402
 from tensorflow.keras import layers  # noqa: E402
 
 
-def get_prediction_subnet(inputs):
-    conc = layers.Concatenate(axis=2)(inputs)
-    dense1 = layers.Dense(128, activation="relu")(conc)
-    dense2 = layers.Dense(64, activation="relu")(dense1)
-    return layers.Dense(3)(dense2)
+def prediction_subnet(selection, other_inputs, name="joint"):
+    conc = layers.Concatenate(axis=2, name=f"{name}_concat")(
+        [selection] + other_inputs)
+    dense1 = layers.Dense(128, activation="relu", name=f"{name}_dense1")(conc)
+    dense2 = layers.Dense(64, activation="relu", name=f"{name}_dense2")(dense1)
+    linear = layers.Dense(3, name=f"{name}_linear")(dense2)
+    return layers.Multiply(name=f"{name}_multiply")([linear, selection])
 
 
 def get_human_model():
@@ -31,48 +32,71 @@ def get_human_model():
     # Dropout on pose input (avoid overfitting)
     pose_dropout = layers.Dropout(0.1, name="pose_dropout")(pose_norm)
     # Concatenate all inputs
-    concat_inputs = layers.Concatenate(axis=2, name="concat_inputs")(
-        [pose_dropout, selection_input, time_input])
+    concat_inputs = layers.Concatenate(axis=2, name="inputs_concat")(
+        [selection_input, pose_dropout, time_input])
     # LSTM layer with dropout
     seq_output = layers.LSTM(2048, return_sequences=True, name="LSTM")(
         concat_inputs)
     seq_dropout = layers.Dropout(0.2, name="LSTM_dropout")(seq_output)
     # Prediction layers
-    neurons = 128
     delta = []
     # Prediction layer: root orientation
     s0 = selection_input[:, :, :3]
-    linear0 = get_prediction_subnet([seq_dropout, s0, time_input])
-    delta.append(layers.Multiply()([linear0, s0]))
+    delta.append(prediction_subnet(s0, [seq_dropout, time_input],
+                                   name="root"))
     # Prediction layer: left hip
     s1 = selection_input[:, :, 3:6]
-    linear1 = get_prediction_subnet([seq_dropout, s1, time_input,
-                                     delta[0]])
-    delta.append(layers.Multiply()([linear1, s1]))
+    delta.append(prediction_subnet(s1, [seq_dropout, time_input,
+                                        delta[0]], name="hipL"))
     # Prediction layer: right hip
     s2 = selection_input[:, :, 6:9]
-    linear2 = get_prediction_subnet([seq_dropout, s2, time_input,
-                                     delta[0]])
-    delta.append(layers.Multiply()([linear2, s2]))
+    delta.append(prediction_subnet(s2, [seq_dropout, time_input,
+                                        delta[0]], name="hipR"))
     # Prediction layer: lower spine
     s3 = selection_input[:, :, 9:12]
-    linear3 = get_prediction_subnet([seq_dropout, s3, time_input,
-                                     delta[0]])
-    delta.append(layers.Multiply()([linear3, s3]))
+    delta.append(prediction_subnet(s3, [seq_dropout, time_input,
+                                        delta[0]], name="spine3"))
     # Prediction layer: left knee
     s4 = selection_input[:, :, 12:15]
-    linear4 = get_prediction_subnet([seq_dropout, s4, time_input,
-                                     delta[0], delta[1]])
-    delta.append(layers.Multiply()([linear4, s4]))
+    delta.append(prediction_subnet(s4, [seq_dropout, time_input,
+                                        delta[0], delta[1]], name="kneeL"))
     # Prediction layer: right knee
     s5 = selection_input[:, :, 15:18]
-    linear5 = get_prediction_subnet([seq_dropout, s5, time_input,
-                                     delta[0], delta[2]])
-    delta.append(layers.Multiply()([linear5, s5]))
+    delta.append(prediction_subnet(s5, [seq_dropout, time_input,
+                                        delta[0], delta[2]], name="kneeR"))
+    # Prediction layer: mid spine
+    s6 = selection_input[:, :, 18:21]
+    delta.append(prediction_subnet(s6, [seq_dropout, time_input,
+                                        delta[0], delta[3]], name="spine6"))
+    # Prediction layer: left ankle
+    s7 = selection_input[:, :, 21:24]
+    delta.append(prediction_subnet(s7, [seq_dropout, time_input,
+                                        delta[0], delta[1], delta[4]],
+                                   name="ankleL"))
+    # Prediction layer: right ankle
+    s8 = selection_input[:, :, 24:27]
+    delta.append(prediction_subnet(s8, [seq_dropout, time_input,
+                                        delta[0], delta[2], delta[5]],
+                                   name="ankleR"))
+    # Prediction layer: high spine
+    s9 = selection_input[:, :, 27:30]
+    delta.append(prediction_subnet(s9, [seq_dropout, time_input,
+                                        delta[0], delta[3], delta[6]],
+                                   name="spine9"))
+    # Prediction layer: left foot
+    s10 = selection_input[:, :, 30:33]
+    delta.append(prediction_subnet(s10, [seq_dropout, time_input, delta[0],
+                                         delta[1], delta[4], delta[7]],
+                                   name="footL"))
+    # Prediction layer: right foot
+    s11 = selection_input[:, :, 33:36]
+    delta.append(prediction_subnet(s11, [seq_dropout, time_input, delta[0],
+                                         delta[2], delta[5], delta[8]],
+                                   name="footR"))
     # Concatenate all predictions
     deltas = layers.Concatenate(axis=2)(delta)
     # Final prediction: pose_input + deltas
-    x = layers.Add()([pose_input[:, :, :15], deltas])
+    x = layers.Add()([pose_input[:, :, :36], deltas])
     # Create the model
     model = keras.Model(inputs=[pose_input, selection_input, time_input],
                         outputs=[x])
@@ -81,4 +105,4 @@ def get_human_model():
 
 if __name__ == "__main__":
     model = get_human_model()
-    keras.utils.plot_model(model)
+    keras.utils.plot_model(model, show_shapes=False)
