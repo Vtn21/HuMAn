3ea9d2b8df06cc7c07a364da1ba4f17ebf5d294f
Vtn21 <victortn21@hotmail.com>
2021-02-12 09:32:16 -0300

    Adding function for the sub-networks

--- model/human.py
+++ model/human.py
@@ -14,13 +14,22 @@ from tensorflow import keras  # noqa: E402
 from tensorflow.keras import layers  # noqa: E402
 
 
+def get_prediction_subnet(inputs):
+    conc = layers.Concatenate(axis=2)(inputs)
+    dense1 = layers.Dense(128, activation="relu")(conc)
+    dense2 = layers.Dense(64, activation="relu")(dense1)
+    return layers.Dense(3)(dense2)
+
+
 def get_human_model():
     # Input layers
     pose_input = keras.Input(shape=(None, 72), name="pose_input")
     selection_input = keras.Input(shape=(None, 72), name="selection_input")
     time_input = keras.Input(shape=(None, 1), name="time_input")
+    # Normalize the pose inputs
+    pose_norm = layers.BatchNormalization(axis=2)(pose_input)
     # Dropout on pose input (avoid overfitting)
-    pose_dropout = layers.Dropout(0.1, name="pose_dropout")(pose_input)
+    pose_dropout = layers.Dropout(0.1, name="pose_dropout")(pose_norm)
     # Concatenate all inputs
     concat_inputs = layers.Concatenate(axis=2, name="concat_inputs")(
         [pose_dropout, selection_input, time_input])
@@ -33,38 +42,33 @@ def get_human_model():
     delta = []
     # Prediction layer: root orientation
     s0 = selection_input[:, :, :3]
-    conc0 = layers.Concatenate(axis=2)([seq_dropout, s0, time_input])
-    dense0 = layers.Dense(neurons, activation="relu")(conc0)
-    linear0 = layers.Dense(3)(dense0)
+    linear0 = get_prediction_subnet([seq_dropout, s0, time_input])
     delta.append(layers.Multiply()([linear0, s0]))
     # Prediction layer: left hip
     s1 = selection_input[:, :, 3:6]
-    conc1 = layers.Concatenate(axis=2)([seq_dropout, s1, time_input,
-                                        delta[0]])
-    dense1 = layers.Dense(neurons, activation="relu")(conc1)
-    linear1 = layers.Dense(3)(dense1)
+    linear1 = get_prediction_subnet([seq_dropout, s1, time_input,
+                                     delta[0]])
     delta.append(layers.Multiply()([linear1, s1]))
     # Prediction layer: right hip
     s2 = selection_input[:, :, 6:9]
-    conc2 = layers.Concatenate(axis=2)([seq_dropout, s2, time_input,
-                                        delta[0]])
-    dense2 = layers.Dense(neurons, activation="relu")(conc2)
-    linear2 = layers.Dense(3)(dense2)
+    linear2 = get_prediction_subnet([seq_dropout, s2, time_input,
+                                     delta[0]])
     delta.append(layers.Multiply()([linear2, s2]))
     # Prediction layer: lower spine
     s3 = selection_input[:, :, 9:12]
-    conc3 = layers.Concatenate(axis=2)([seq_dropout, s3, time_input,
-                                        delta[0]])
-    dense3 = layers.Dense(neurons, activation="relu")(conc3)
-    linear3 = layers.Dense(3)(dense3)
+    linear3 = get_prediction_subnet([seq_dropout, s3, time_input,
+                                     delta[0]])
     delta.append(layers.Multiply()([linear3, s3]))
     # Prediction layer: left knee
     s4 = selection_input[:, :, 12:15]
-    conc4 = layers.Concatenate(axis=2)([seq_dropout, s4, time_input,
-                                        delta[0], delta[1]])
-    dense4 = layers.Dense(neurons, activation="relu")(conc4)
-    linear4 = layers.Dense(3)(dense4)
+    linear4 = get_prediction_subnet([seq_dropout, s4, time_input,
+                                     delta[0], delta[1]])
     delta.append(layers.Multiply()([linear4, s4]))
+    # Prediction layer: right knee
+    s5 = selection_input[:, :, 15:18]
+    linear5 = get_prediction_subnet([seq_dropout, s5, time_input,
+                                     delta[0], delta[2]])
+    delta.append(layers.Multiply()([linear5, s5]))
     # Concatenate all predictions
     deltas = layers.Concatenate(axis=2)(delta)
     # Final prediction: pose_input + deltas
