8972ed3d0b17d7be9dca635fdc58ce8af153376c
Vtn21 <victortn21@hotmail.com>
2021-02-25 18:02:53 -0300

    Dense layers with tanh activation

--- human/model/human.py
+++ human/model/human.py
@@ -22,10 +22,10 @@ L2_PENALTY = 0.001
 
 def prediction_subnet(selection, inputs, name="joint"):
     conc = layers.Concatenate(axis=2, name=f"{name}_concat")(inputs)
-    dense1 = layers.Dense(64, activation="relu", name=f"{name}_dense1",
+    dense1 = layers.Dense(64, activation="tanh", name=f"{name}_dense1",
                           kernel_regularizer=L2(L2_PENALTY))(conc)
     dropout1 = layers.Dropout(0.1, name=f"{name}_dropout1")(dense1)
-    dense2 = layers.Dense(32, activation="relu", name=f"{name}_dense2",
+    dense2 = layers.Dense(32, activation="tanh", name=f"{name}_dense2",
                           kernel_regularizer=L2(L2_PENALTY))(dropout1)
     dropout2 = layers.Dropout(0.1, name=f"{name}_dropout2")(dense2)
     linear = layers.Dense(3, name=f"{name}_linear")(dropout2)
@@ -188,3 +188,4 @@ def get_human_model(pose_input_dataset=None):
 if __name__ == "__main__":
     model = get_human_model()
     keras.utils.plot_model(model, show_shapes=True)
+    model.summary()
