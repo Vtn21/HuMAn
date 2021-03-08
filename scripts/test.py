7f6bfcaa2cba10843177e7de20d76d7021f32f8e
Vtn21 <victortn21@hotmail.com>
2021-03-08 18:01:03 -0300

    Updated testing visualizer

--- scripts/test.py
+++ scripts/test.py
@@ -3,87 +3,51 @@
 Author: Victor T. N.
 """
 
-import glob
+
+import numpy as np
 import os
+from human.model.human import HuMAn
 from human.utils import dataset
 from human.utils.visualization import view_test
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "1"  # Hide unnecessary TF messages
 import tensorflow as tf  # noqa: E402
 
-# TODO: create a new testing routine. Autoregression is no longer needed.
-# Use code below as reference.
 
-# From old test.py
+SHUFFLE_BUFFER = 1000
+
+
 if __name__ == "__main__":
-    # Load the datasets
     # Path where the TFRecords are located
     tfr_home = "../../AMASS/tfrecords"
-    # Load the TFRecords into datasets
-    parsed_ds = dataset.load_all_splits(tfr_home)
+    # Load the testing dataset
+    parsed_ds = dataset.load_splits(tfr_home, splits=["test"])
     # Load the test data
     test_ds = (parsed_ds["test"]
                .map(dataset.map_test,
-                    num_parallel_calls=tf.data.AUTOTUNE)
-               .shuffle(1000)
+                    num_parallel_calls=tf.data.AUTOTUNE,
+                    deterministic=False)
+               .shuffle(SHUFFLE_BUFFER)
                .batch(1)
                .prefetch(tf.data.AUTOTUNE))
     test_iter = iter(test_ds)
-    # Load the trained model from checkpoint
-    ckpt_path = "checkpoints/*"
-    ckpt_dirs = sorted(glob.glob(ckpt_path))
-    model = tf.keras.models.load_model(ckpt_dirs[-1])
+    # The HuMAn neural network
+    # Expects normalization layer adapted, as model is trained
+    model = HuMAn()
+    # Load weights from checkpoints
+    ckpt_path = "checkpoints"
+    latest_ckpt = tf.train.latest_checkpoint(ckpt_path)
+    model.load_weights(latest_ckpt)
     # Make a prediction
-    inputs, aux = next(test_iter)
+    inputs, pose_target, aux = next(test_iter)
     prediction = model.predict(inputs)
+    framerate = np.round(1/aux["dt"].numpy().item(), decimals=1)
+    horizon = np.round(inputs['horizon_input'][0, 0].numpy().item(),
+                       decimals=4)
+    print(f"Input framerate is {framerate} Hz.")
+    print(f"Prediction horizon is {horizon} s.")
     view_test(path_star="../../AMASS/models/star",
-              poses_prediction=prediction[0, :-1, :],
-              poses_ground_truth=inputs["pose_input"][0, 1:, :],
-              betas=aux["betas"][0, :, :],
+              poses_prediction=prediction[0],
+              poses_ground_truth=pose_target[0],
+              betas=aux["betas"][0],
               dt=aux["dt"].numpy().item(),
               gender=tf.compat.as_str(aux["gender"].numpy().item()))
-
-
-# From old "test_autoregressive.py"
-if __name__ == "__main__":
-    # Load the datasets
-    # Path where the TFRecords are located
-    tfr_home = "../../AMASS/tfrecords"
-    # Load the TFRecords into datasets
-    parsed_ds = dataset.load_all_splits(tfr_home)
-    # Load the test data
-    test_ds = (parsed_ds["test"]
-               .map(dataset.map_test,
-                    num_parallel_calls=tf.data.AUTOTUNE)
-               .shuffle(1000)
-               .batch(1)
-               .prefetch(tf.data.AUTOTUNE))
-    test_iter = iter(test_ds)
-    # Load the trained model from checkpoint
-    ckpt_path = "checkpoints/*"
-    ckpt_dirs = sorted(glob.glob(ckpt_path))
-    model = tf.keras.models.load_model(ckpt_dirs[-1])
-    # Split the inputs
-    inputs, aux = next(test_iter)
-    # model.predict(inputs)
-    # Make the LSTM layer stateful
-    model.get_layer("LSTM").stateful = True
-    # Select a frame to start autoregressive prediction
-    i_autoregressive = 100
-    prediction_sequence = []
-    for i in range(inputs["pose_input"].shape[1]):
-        inputs_step = {"selection_input": inputs["selection_input"][0:1, i:i+1],
-                       "time_input": inputs["time_input"][0:1, i:i+1]}
-        if i < i_autoregressive:
-            inputs_step["pose_input"] = inputs["pose_input"][0:1, i:i+1]
-        else:
-            inputs_step["pose_input"] = tf.reshape(prediction, [1, 1, 72])
-        prediction = model.predict(inputs_step)
-        prediction_sequence.append(prediction)
-    poses_prediction = tf.reshape(tf.constant(prediction_sequence), (-1, 72))
-    view_test(path_star="../../AMASS/models/star",
-              poses_prediction=poses_prediction[:-1],
-              poses_ground_truth=inputs["pose_input"][0, 1:, :],
-              betas=aux["betas"][0, :, :],
-              dt=aux["dt"].numpy().item(),
-              gender=tf.compat.as_str(aux["gender"].numpy().item()))
-    # model.get_layer("LSTM").reset_states()
