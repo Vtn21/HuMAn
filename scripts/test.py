aed608080b5eb57ebb787704bd1d68938fb69c2c
Vtn21 <victortn21@hotmail.com>
2021-03-19 16:23:02 -0300

    Added plots to testing script

--- scripts/test.py
+++ scripts/test.py
@@ -4,6 +4,7 @@ Author: Victor T. N.
 """
 
 
+import matplotlib.pyplot as plt
 import numpy as np
 import os
 from human.model.human import HuMAn
@@ -20,9 +21,9 @@ if __name__ == "__main__":
     # Path where the TFRecords are located
     tfr_home = "../../AMASS/tfrecords"
     # Load the testing dataset
-    parsed_ds = dataset.load_splits(tfr_home, splits=["test"])
+    parsed_ds = dataset.load_splits(tfr_home, splits=["test_1024"])
     # Load the test data
-    test_ds = (parsed_ds["test"]
+    test_ds = (parsed_ds["test_1024"]
                .map(dataset.map_test,
                     num_parallel_calls=tf.data.AUTOTUNE,
                     deterministic=False)
@@ -45,6 +46,16 @@ if __name__ == "__main__":
                        decimals=4)
     print(f"Input framerate is {framerate} Hz.")
     print(f"Prediction horizon is {horizon} s.")
+
+    fig, axs = plt.subplots(3, 1)
+    axs[0].plot(prediction[0])
+    axs[0].set_ylabel("Prediction")
+    axs[1].plot(pose_target[0])
+    axs[1].set_ylabel("Ground truth")
+    axs[2].plot(np.abs(prediction[0] - pose_target[0]))
+    axs[2].set_ylabel("Absolute error")
+    plt.show()
+
     view_test(path_star="../../AMASS/models/star",
               poses_prediction=prediction[0],
               poses_ground_truth=pose_target[0],
