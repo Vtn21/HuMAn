9237f6feb46da129f2352f7e24eff94ebd3c1b0d
Vtn21 <victortn21@hotmail.com>
2021-04-08 10:07:31 -0300

    Added function to run a complete training loop

---
+++ human/model/train.py
@@ -0,0 +1,65 @@
+"""train.py
+
+Author: Victor T. N.
+"""
+
+
+import os
+from datetime import datetime
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "1"  # Hide unnecessary TF messages
+import tensorflow as tf  # noqa: E402
+import tensorflow_addons as tfa  # noqa: E402
+
+
+def full_training_loop(model, train_datasets=[], valid_datasets=[],
+                       seq_lengths=[], batch_sizes=[], swa=[], rlrp=[],
+                       patience=[], name="noname", ckpt_dir="checkpoints",
+                       log_dir="logs", save_dir="saves"):
+    for i in range(len(train_datasets)):
+        # Retrieve current date and time
+        date = datetime.today().strftime("%Y-%m-%d-%H-%M")
+        # Callbacks list
+        callbacks = []
+        if swa[i]:
+            # Timestamp
+            stamp = f"{date}_{name}_swa"
+            # Optimizer (SGD + SWA)
+            opt = tfa.optimizers.SWA(tf.keras.optimizers.SGD(
+                    learning_rate=1e-5, momentum=0.5, nesterov=True))
+            # Checkpoint callback
+            callbacks.append(tfa.callbacks.AverageModelCheckpoint(
+                update_weights=True, filepath=f"{ckpt_dir}/{stamp}",
+                save_best_only=True, save_weights_only=True))
+        else:
+            # Timestamp
+            stamp = f"{date}_{name}_{seq_lengths[i]}_{batch_sizes[i]}"
+            # Optimizer (Adam)
+            opt = tf.keras.optimizers.Adam(learning_rate=1e-3)
+            # Checkpoint callback
+            callbacks.append(tf.keras.callbacks.ModelCheckpoint(
+                filepath=f"{ckpt_dir}/{stamp}", save_best_only=True,
+                save_weights_only=True))
+        # Early stopping callback
+        callbacks.append(tf.keras.callbacks.EarlyStopping(
+            patience=patience[i]))
+        # TensorBoard callback
+        callbacks.append(tf.keras.callbacks.TensorBoard(
+            log_dir=os.path.join(log_dir, stamp), profile_batch=0))
+        # Reduce learning rate on plateau callback
+        if rlrp[i]:
+            callbacks.append(tf.keras.callbacks.ReduceLROnPlateau(
+                factor=0.2, patience=patience[i]))
+        # Compile the model
+        model.compile(loss="mse", metrics=["mae"], optimizer=opt)
+        # Print useful information
+        print(f"Sequence length: {seq_lengths[i]}\n"
+              f"Batch size: {batch_sizes[i]}")
+        if swa[i]:
+            print("Optimizer: SGD + SWA")
+        else:
+            print("Optimizer: Adam")
+        # Start training
+        model.fit(x=train_datasets[i], epochs=20, callbacks=callbacks,
+                  validation_data=valid_datasets[i])
+    print(f"Training done for {name}. Saving model...")
+    model.save_weights(os.path.join(save_dir, name))
