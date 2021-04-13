0d903090d2e539605a6c47d7070429a43752a934
Vtn21 <victortn21@hotmail.com>
2021-04-13 15:59:44 -0300

    Added multiple skeleton structures to map function

--- human/utils/dataset.py
+++ human/utils/dataset.py
@@ -51,56 +51,87 @@ def tfrecords_to_dataset(tfr_list=[]):
     return raw_ds.map(parse_record)
 
 
-def map_dataset(data, test=False):
-    """A hybrid mapping function, used for defining "map_train" and "map_test"
-    in a concise way and avoid repeating code. It is advised not to call this
-    function directly, choosing between "map_train" and "map_test" instead.
+def map_dataset(data, skeleton="random", horizon_frames=-1):
+    """A dataset mapping function with args. For training, the "map_train"
+    function is provided for easy usage (default args). For evaluating, it is
+    advised to create lambda functions in order to specify the args, and later
+    on use "map" on the dataset.
 
     Args:
         data (tf.data): a parsed sample from the dataset.
-        test (bool, optional): choose betwen testing and training.
-            Defaults to False (training).
+        skeleton (string, optional): the skeleton structure type. Accepts
+                                     "arms", "full_body", "legs", "legs_arms",
+                                     and "random". Defaults to "random".
+        horizon_frame (int, optional): the number of frames to shift in order
+                                       to create the target predictions.
+                                       Defaults to -1, that creates random
+                                       shifts.
 
     Returns:
-        tuple: (dict_inputs, targets) for training, or
-            (dict_inputs, targets, dict_aux) for testing.
+        tuple: (dict_inputs, targets).
     """
-    poses, seq_length, betas, dt, gender = decode_record(data)
+    poses, seq_length, _, dt, _ = decode_record(data)
     pose_input = poses[:seq_length]
     elapsed_input = dt * tf.ones(shape=[seq_length, 1], dtype=tf.float32)
-    # Build a selection vector
-    if test:
-        # All ones
+    # Build a selection vector, according to the selected skeleton structure
+    if skeleton == "arms":
+        selection_vec = tf.concat([tf.zeros(shape=(1, 48)),
+                                   tf.ones(shape=(1, 24))], axis=1)
+        selection_input = tf.tile(selection_vec, [seq_length, 1])
+    elif skeleton == "full_body":
         selection_input = tf.ones(shape=tf.shape(pose_input))
-    else:
-        # Random
+    elif skeleton == "legs":
+        selection_vec = tf.concat([tf.tile(tf.constant(
+            [[0, 0, 0, 1, 1, 1, 1, 1, 1]], dtype=tf.float32), [1, 4]),
+            tf.zeros(shape=(1, 36))], axis=1)
+        selection_input = tf.tile(selection_vec, [seq_length, 1])
+    elif skeleton == "legs_arms":
+        selection_vec = tf.concat([tf.tile(tf.constant(
+            [[0, 0, 0, 1, 1, 1, 1, 1, 1]], dtype=tf.float32), [1, 4]),
+            tf.zeros(shape=(1, 12)), tf.ones(shape=(1, 24))], axis=1)
+        selection_input = tf.tile(selection_vec, [seq_length, 1])
+    elif skeleton == "random":
         rand_unif = tf.random.uniform(shape=[1, 72])
         offset = tf.random.uniform(shape=[], minval=-0.5, maxval=0.5)
         selection_vec = tf.math.round(
             rand_unif + offset*tf.ones(shape=[1, 72]))
         selection_input = tf.tile(selection_vec, [seq_length, 1])
-    # Create a pose target with a random time horizon
-    max_shift = tf.shape(poses, out_type=tf.int64)[0] - seq_length
-    shift = tf.random.uniform(shape=[], minval=1, maxval=max_shift,
-                              dtype=tf.int64)
-    horizon_input = tf.cast(shift, tf.float32)*elapsed_input
-    pose_target = tf.multiply(poses[shift:seq_length+shift], selection_input)
-    inputs = {"pose_input": pose_input,
-              "selection_input": selection_input,
-              "elapsed_input": elapsed_input,
-              "horizon_input": horizon_input}
-    # Auxiliary data for testing purposes
-    aux = {"betas": betas, "dt": dt, "gender": gender}
-    if test:
-        return inputs, pose_target, aux
     else:
-        return inputs, pose_target
+        return ValueError("Invalid skeleton configuration. Valid choices:"
+                          "arms, full_body, legs, legs_arms, random.")
+    # Compute the maximum allowed prediction horizon frame shift
+    max_shift = tf.shape(poses, out_type=tf.int64)[0] - seq_length
+    # Set the desired prediction horizon, in frames
+    if horizon_frames == -1:
+        # Create a pose target with a random time horizon
+        shift = tf.random.uniform(shape=[], minval=1, maxval=max_shift,
+                                  dtype=tf.int64)
+    else:
+        # Fixed prediction horizon (input-defined)
+        shift = horizon_frames
+    if horizon_frames > max_shift:
+        # Return "None" as a warning (not enough frames to shift)
+        inputs = {"pose_input": pose_input,
+                  "selection_input": selection_input,
+                  "elapsed_input": elapsed_input,
+                  "horizon_input": tf.zeros(shape=[], dtype=tf.float32)}
+        pose_target = pose_input
+    else:
+        pose_target = tf.multiply(poses[shift:seq_length+shift],
+                                  selection_input)
+        horizon_input = tf.cast(shift, tf.float32)*elapsed_input
+        inputs = {"pose_input": pose_input,
+                  "selection_input": selection_input,
+                  "elapsed_input": elapsed_input,
+                  "horizon_input": horizon_input}
+    return inputs, pose_target
 
 
 def map_train(data):
     """Map a parsed dataset into inputs (pose, selection, elapsed and horizon)
     and targets (pose). Call it using the dataset.map method, for training
-    purposes.
+    purposes. It calls "map_dataset" with default args, which randomizes
+    skeleton structure and prediction horizon.
 
     Args:
         data (tf.data): a parsed sample from the dataset.
@@ -109,20 +140,6 @@ def map_train(data):
         tuple: (dict_inputs, targets)
     """
     return map_dataset(data)
-
-
-def map_test(data):
-    """Map a parsed dataset into inputs (pose, selection, elapsed and horizon),
-    targets (pose) and auxiliary values (betas, dt and gender). Call it using
-    the dataset.map method, for testing purposes.
-
-    Args:
-        data (tf.data): a parsed sample from the dataset.
-
-    Returns:
-        tuple: (dict_inputs, targets, dict_aux)
-    """
-    return map_dataset(data, test=True)
 
 
 def map_pose_input(data):
